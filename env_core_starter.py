450257d35d0bce0e06974f2bbe55a16d3956e037
lastrise <54949052+lastrise@users.noreply.github.com>
2023-05-31 21:20:19 +0300

    Add files via upload

---
+++ env_core_starter.py
@@ -0,0 +1,19 @@
+import os
+
+args = {'rpcuser': os.getenv("RPC_USER", "admin"),
+        'rpcpassword': os.getenv("RPC_PASSWORD", "admin"),
+        'rpcbind': os.getenv("RPC_BIND", "0.0.0.0").split(";"),
+        'rpcallowip': os.getenv("RPC_ALLOW_IP", "0.0.0.0").split(";")}
+
+CORE_PATH = f"/usr/local/bin/{os.getenv('CORE_NAME')}d"
+
+args_list = []
+for key in args:
+    value = args.get(key)
+    if isinstance(value, list):
+        for element in value:
+            args_list.append(f"-{key}={element}")
+    else:
+        args_list.append(f"-{key}={value}")
+print(args_list)
+os.execl(CORE_PATH, *args_list)
